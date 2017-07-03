3f9a5b7ad1c8c5ba8ca2658d58e4e9466409f1f6
NORTHAMERICA\t-nihoer <t-nihoer@microsoft.com>
2017-07-03 12:58:15 -0400

    initial commit for poisson model

---
+++ sklearn/linear_model/poisson.py
@@ -0,0 +1,91 @@
+"""
+Poisson Regression
+"""
+
+import numbers
+import warnings
+
+import numpy as np
+from scipy import optimize, sparse
+
+from .base import LinearModel, RegressorMixin
+
+
+def _log_likelihood():
+    '''
+    describes the log likelihood of the poisson regression
+    '''
+
+def _grad_log_likelihood():
+    """
+    Gradient of the log likelihood used for finding the MLE
+    """
+
+def _hessian_log_likelihood():
+    """
+    Hessian used in the optimisation of the MLE
+    """
+
+class PoissonRegression(LinearModel, RegressorMixin):
+    """    
+    Poisson Regression:
+    TODO: Description here of the actual poisson module
+
+    Parameters:
+    -----------
+    TODO: optimisers to use, penalty to use, penalty param, max_iter
+
+    Attributes:
+    ----------
+    
+    coef_ : array, shape (1, n_features) or (n_classes, n_features)
+        Coefficient of the features in the decision function.
+        `coef_` is of shape (1, n_features) when the given problem
+        is binary.
+    intercept_ : array, shape (1,) or (n_classes,)
+        Intercept (a.k.a. bias) added to the decision function.
+        If `fit_intercept` is set to False, the intercept is set to zero.
+        `intercept_` is of shape(1,) when the problem is binary.
+    n_iter_ : array, shape (n_classes,) or (1, )
+
+    See also:
+    ---------
+
+    Notes:
+    ---------
+
+    References:
+    ----------
+    
+    """
+
+    def __init__(self, tol=1e-4, alpha=0., fit_intercept=False,
+                 solver='sag', max_iter=1000, verbose=0):
+        self.tol = tol
+        self.alpha = alpha
+        self.fit_intercept = fit_intercept
+        self.solver = solver
+        self.max_iter = max_iter
+        self.verbose = verbose
+
+    def fit(self, X, y, exposure=None):
+        """Fit the model according to the given training data.
+        Parameters
+        ----------
+        """
+    
+    def predict(self, X):
+        """Probability estimates.
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+        Returns
+        -------
+        T : array-like, shape = [n_samples, n_classes]
+            Returns the probability of the sample for each class in the model,
+            where classes are ordered as they are in ``self.classes_``.
+        """
+        if not hasattr(self, "coef_"):
+            raise NotFittedError("Call fit before prediction")
+
+        assert False
